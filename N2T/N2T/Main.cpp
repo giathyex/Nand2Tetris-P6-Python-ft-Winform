e8ec997cb90fb84488b8005fa95508d427c9740d
Trần Đình Gia Hải <60392917+giathyex@users.noreply.github.com>
2020-12-14 02:40:14 +0700

    Add files via upload

---
+++ N2T/N2T/Main.cpp
@@ -0,0 +1,13 @@
+#include "Main.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThread]
+void main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	N2T::Main form;
+	Application::Run(%form);
+}
+
---
+++ N2T/N2T/Main.h
@@ -0,0 +1,210 @@
+﻿#pragma once
+
+namespace N2T {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+	using namespace System;
+	using namespace System::IO;
+	using namespace System::Diagnostics;
+
+	/// <summary>
+	/// Summary for Main
+	/// </summary>
+	public ref class Main : public System::Windows::Forms::Form
+	{
+	public:
+		String ^ filename = "nhh";
+		Main(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~Main()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::PictureBox^  pictureBox1;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::Label^  label3;
+	private: System::Windows::Forms::Label^  label4;
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(Main::typeid));
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::Color::Salmon;
+			this->button1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button1->Location = System::Drawing::Point(38, 315);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(250, 40);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Convert to machine code";
+			this->button1->UseVisualStyleBackColor = false;
+			this->button1->Click += gcnew System::EventHandler(this, &Main::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->BackColor = System::Drawing::Color::MistyRose;
+			this->button2->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button2->Location = System::Drawing::Point(38, 267);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(250, 40);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"Select .ASM file";
+			this->button2->UseVisualStyleBackColor = false;
+			this->button2->Click += gcnew System::EventHandler(this, &Main::button2_Click);
+			// 
+			// label1
+			// 
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->ForeColor = System::Drawing::Color::Maroon;
+			this->label1->ImageAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			this->label1->Location = System::Drawing::Point(33, 15);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(255, 109);
+			this->label1->TabIndex = 2;
+			this->label1->Text = L"ASSEMBLER \r\nFOR NAND2TETRIS";
+			this->label1->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(241, -7);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(300, 413);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
+			this->pictureBox1->TabIndex = 3;
+			this->pictureBox1->TabStop = false;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->ForeColor = System::Drawing::Color::Maroon;
+			this->label2->Location = System::Drawing::Point(38, 110);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(128, 15);
+			this->label2->TabIndex = 4;
+			this->label2->Text = L"By N PERFORMANCE";
+			// 
+			// label3
+			// 
+			this->label3->BackColor = System::Drawing::Color::Transparent;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->ForeColor = System::Drawing::Color::Black;
+			this->label3->Location = System::Drawing::Point(38, 183);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(250, 52);
+			this->label3->TabIndex = 5;
+			this->label3->Text = L"No file selected";
+			this->label3->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			// 
+			// label4
+			// 
+			this->label4->ForeColor = System::Drawing::Color::Maroon;
+			this->label4->Location = System::Drawing::Point(38, 235);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(250, 29);
+			this->label4->TabIndex = 6;
+			this->label4->Text = L"________________________________________";
+			this->label4->TextAlign = System::Drawing::ContentAlignment::TopCenter;
+			// 
+			// Main
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::Color::FloralWhite;
+			this->ClientSize = System::Drawing::Size(504, 401);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->pictureBox1);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
+			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
+			this->MaximizeBox = false;
+			this->Name = L"Main";
+			this->Padding = System::Windows::Forms::Padding(5);
+			this->Text = L"Assembler by N PERFORMANCE";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) 
+	{
+				 if (filename != "nhh")
+				 {
+					 StreamWriter ^ outFile = gcnew StreamWriter("nhh.txt");
+					 outFile->Write(filename);
+					 outFile->Close();
+					 label3->Text = "Convert successfully";
+					 Process::Start("assembler.exe");
+				 }
+				 else
+				 {
+					 label3->Text = "Please select file first";
+				 }
+	}
+	private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e)
+	{
+				 OpenFileDialog ^ openfile1 = gcnew OpenFileDialog;
+				 if (openfile1->ShowDialog() == System::Windows::Forms::DialogResult::OK)
+				 {
+					 filename = openfile1->FileName;
+					 label3->Text = filename;
+				 }
+	}
+	};
+}
